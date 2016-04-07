d0625f66857910765f50ae95b64c2ec597e81d80
Kumori <kumori@ryougi.com>
2016-04-07 19:47:38 -0400

    split project into modules

--- data.lua
+++ data.lua
@@ -1,37 +1,3 @@
 
 
 require("prototypes.sand") -- New Raw Resources
-
---Support for bobs mods
---if bobmods and bobmods.config and bOBmods.config.ores then
---  if bobmods.config.ores.EnableBauxite then
---    require("prototypes.bauxite")
---  end
---  if bobmods.config.ores.EnableCobaltOre then
---    require("prototypes.cobalt")
---  end
---  if bobmods.config.ores.EnableGemsOre then
---    require("prototypes.cobalt")
---  end
---  if bobmods.config.ores.EnableGoldOre then
---    require("prototypes.gold")
---  end
---  if bobmods.config.ores.EnableLeadOre then
---    require("prototypes.lead")
---  end
---  if bobmods.config.ores.EnableNickelOre then
---    require("prototypes.nickel")
---  end
---  if bobmods.config.ores.EnableRutile then
---    require("prototypes.rutile")
---  end
---  if bobmods.config.ores.EnableSilverOre then
---    require("prototypes.silver")
---  end
---  if bobmods.config.ores.EnableTinOre then
---    require("prototypes.tin")
---  end
---  if bobmods.config.ores.EnableZincOre then
---    require("prototypes.zinc")
---  end
---end
