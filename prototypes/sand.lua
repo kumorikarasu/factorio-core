aca6d128693be38f1d50349e2a30c0c82e767f04
Kumori <kumori@ryougi.com>
2016-04-09 13:05:12 -0400

    Added sand, updated gravel image

---
+++ prototypes/sand.lua
@@ -0,0 +1,14 @@
+
+data:extend(
+{
+  {
+  type = "item",
+  name = "sand",
+  icon = "__kumori-core__/graphics/sand/sand.png",
+  flags = {"goes-to-main-inventory"},
+  subgroup = "raw-material",
+  order = "f",
+  stack_size = 200
+  }
+}
+)
