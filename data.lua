8df02dc53bc0e6385f9ad06c24577696cbb100cd
Kumori <kumori@ryougi.com>
2016-04-10 17:09:11 -0400

    added sand include

--- data.lua
+++ data.lua
@@ -2,3 +2,11 @@
 
 require("prototypes.gravel")
 require("prototypes.sand") -- New Raw Resources
+
+
+
+
+
+--Disable and Modify some of the early game tech to make it a little harder
+
+table.insert(data.raw["technology"]["steel-processing"].effects, {type = "unlock-recipe", recipe = "pig-iron"})
