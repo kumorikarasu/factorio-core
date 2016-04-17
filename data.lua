150d62c4fa5d16d040a78430401728a0f630e6fc
Kumori <kumori@ryougi.com>
2016-04-17 13:11:22 -0400

    added clay resource

--- data.lua
+++ data.lua
@@ -2,6 +2,7 @@
 
 require("prototypes.gravel")
 require("prototypes.sand") -- New Raw Resources
+require("prototypes.clay") -- New Raw Resources
 
 
 
---
+++ prototypes/clay.lua
@@ -0,0 +1,14 @@
+
+data:extend(
+{
+  {
+  type = "item",
+  name = "clay",
+  icon = "__kumori-core__/graphics/clay/clay.png",
+  flags = {"goes-to-main-inventory"},
+  subgroup = "raw-material",
+  order = "f",
+  stack_size = 200
+  }
+}
+)
